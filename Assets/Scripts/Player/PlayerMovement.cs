df6242902490c32e4f694022f8614eb8e633b594
Summoner-jpg <friassimon024@gmail.com>
2024-11-14 17:24:22 -0300

    avance
    se corrigió la fricción con las paredes y el salto(ya que se podía saltar una vez mas al chocar con una pared) además se agregó una animación de caminar y un spawn point

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    
     private bool _isGrounded;
     private Rigidbody2D _rb;
     private Vector2 _moveDirection;
-
+    private Animator _animator;
     private PlayerRoll _playerRoll;
 
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
         if (_rb == null)
         {
@@ -25,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && !_playerRoll.IsRolling)
         {
@@ -35,13 +42,26 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+        
+        if (Mathf.Abs(_moveDirection.x) > 0)
+        {
+            _animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            _animator.SetBool("IsMoving", false);
+        }
     }
-
+    
     void FixedUpdate()
     {
         if (_moveDirection.magnitude > 0 && !_playerRoll.IsRolling)
         {
             _rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _rb.velocity.y);
+        }
+        else if (!_playerRoll.IsRolling)
+        {
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
         }
     }
 
@@ -49,24 +69,4 @@ public class PlayerMovement : MonoBehaviour
     {
         _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = false;
-        }
-    }
-}    
-    
-
-
-
+}
